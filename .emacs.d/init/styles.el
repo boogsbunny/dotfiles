3b52fb48bac50dda0813fd57050b262e790a23a3
babdulkarim <ba2ra@virginia.edu>
2020-03-31 10:17:39 -0400

    added transparency layer as toggle functionality

--- .emacs.d/init/styles.el
+++ .emacs.d/init/styles.el
@@ -14,3 +14,19 @@
   :ensure t)
 (load-theme 'airline-doom-one t)
 (setq powerline-height 30)
+
+;; transparency
+;; (set-frame-parameter (selected-frame) 'alpha '(90 . 50))
+;; (add-to-list 'default-frame-alist '(alpha . (90 . 50)))
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+                    ((numberp (cdr alpha)) (cdr alpha))
+                    ;; Also handle undocumented (<active> <inactive>) form.
+                    ((numberp (cadr alpha)) (cadr alpha)))
+              100)
+         '(85 . 50) '(100 . 100)))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
