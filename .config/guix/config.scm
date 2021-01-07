d7484fc7e96a84bb8e1abc522a7c1b9193758e15
Bugi Abdulkarim <babdulkarim@alarm.com>
2021-01-07 15:48:32 -0500

    added additional drive

--- .config/guix/config.scm
+++ .config/guix/config.scm
@@ -1,88 +1,144 @@
 (use-modules (gnu) (nongnu packages linux))
 (use-service-modules desktop networking ssh xorg)
 
-(define-module (boogs linux-custom)
-               #:use-module (guix gexp)
-               #:use-module (guix packages)
-               #:use-module (guix download)
-               #:use-module (guix git-download)
-               #:use-module (guix build-system trivial)
-               #:use-module ((guix licenses) #:prefix license:)
-               #:use-module (gnu packages linux)
-               #:use-module (srfi srfi-1))
-
-(define-public linux-nonfree
-               (package
-                 (inherit linux-libre)
-                 (name "linux-nonfree")
-                 (version (package-version linux-libre))
-                 (source
-                   (origin
-                     (method url-fetch)
-                     (uri
-                       (string-append
-                         "https://www.kernel.org/pub/linux/kernel/v4.x/"
-                         "linux-" version ".tar.xz"))
-                     (sha256
-                       (base32
-                         "1lm2s9yhzyqra1f16jrjwd66m3jl43n5k7av2r9hns8hdr1smmw4"))))))
-
-(define (linux-firmware-version) "9d40a17beaf271e6ad47a5e714a296100eef4692")
-(define (linux-firmware-source version)
-  (origin
-    (method git-fetch)
-    (uri (git-reference
-           (url (string-append "https://git.kernel.org/pub/scm/linux/kernel"
-                               "/git/firmware/linux-firmware.git"))
-           (commit version)))
-    (file-name (string-append "linux-firmware-" version "-checkout"))
-    (sha256
-      (base32
-        "099kll2n1zvps5qawnbm6c75khgn81j8ns0widiw0lnwm8s9q6ch"))))
-
-(define-public linux-firmware-iwlwifi
-               (package
-                 (name "linux-firmware-iwlwifi")
-                 (version (linux-firmware-version))
-                 (source (linux-firmware-source version))
-                 (build-system trivial-build-system)
-                 (arguments
-                   `(#:modules ((guix build utils))
-                     #:builder (begin
-                                 (use-modules (guix build utils))
-                                 (let ((source (assoc-ref %build-inputs "source"))
-                                       (fw-dir (string-append %output "/lib/firmware/")))
-                                   (mkdir-p fw-dir)
-                                   (for-each (lambda (file)
-                                               (copy-file file
-                                                          (string-append fw-dir (basename file))))
-                                             (find-files source
-                                                         "iwlwifi-.*\\.ucode$|LICENSE\\.iwlwifi_firmware$"))
-                                   #t))))
-                 (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
-                 (synopsis "Non-free firmware for Intel wifi chips")
-                 (description "Non-free iwlwifi firmware")
-                 (license (license:non-copyleft
-                            "https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/tree/LICENCE.iwlwifi_firmware?id=HEAD"))))
+;;(define-module (boogs linux-custom)
+;;               #:use-module (guix gexp)
+;;               #:use-module (guix packages)
+;;               #:use-module (guix download)
+;;               #:use-module (guix git-download)
+;;               #:use-module (guix build-system trivial)
+;;               #:use-module ((guix licenses) #:prefix license:)
+;;               #:use-module (gnu packages linux)
+;;               #:use-module (srfi srfi-1))
+;;
+;;(define-public linux-nonfree
+;;               (package
+;;                 (inherit linux-libre)
+;;                 (name "linux-nonfree")
+;;                 (version (package-version linux-libre))
+;;                 (source
+;;                   (origin
+;;                     (method url-fetch)
+;;                     (uri
+;;                       (string-append
+;;                         "https://www.kernel.org/pub/linux/kernel/v4.x/"
+;;                         "linux-" version ".tar.xz"))
+;;                     (sha256
+;;                       (base32
+;;                         "1lm2s9yhzyqra1f16jrjwd66m3jl43n5k7av2r9hns8hdr1smmw4"))))))
+;;
+;;(define (linux-firmware-version) "9d40a17beaf271e6ad47a5e714a296100eef4692")
+;;(define (linux-firmware-source version)
+;;  (origin
+;;    (method git-fetch)
+;;    (uri (git-reference
+;;           (url (string-append "https://git.kernel.org/pub/scm/linux/kernel"
+;;                               "/git/firmware/linux-firmware.git"))
+;;           (commit version)))
+;;    (file-name (string-append "linux-firmware-" version "-checkout"))
+;;    (sha256
+;;      (base32
+;;        "099kll2n1zvps5qawnbm6c75khgn81j8ns0widiw0lnwm8s9q6ch"))))
+;;
+;;(define-public linux-firmware-iwlwifi
+;;               (package
+;;                 (name "linux-firmware-iwlwifi")
+;;                 (version (linux-firmware-version))
+;;                 (source (linux-firmware-source version))
+;;                 (build-system trivial-build-system)
+;;                 (arguments
+;;                   `(#:modules ((guix build utils))
+;;                     #:builder (begin
+;;                                 (use-modules (guix build utils))
+;;                                 (let ((source (assoc-ref %build-inputs "source"))
+;;                                       (fw-dir (string-append %output "/lib/firmware/")))
+;;                                   (mkdir-p fw-dir)
+;;                                   (for-each (lambda (file)
+;;                                               (copy-file file
+;;                                                          (string-append fw-dir (basename file))))
+;;                                             (find-files source
+;;                                                         "iwlwifi-.*\\.ucode$|LICENSE\\.iwlwifi_firmware$"))
+;;                                   #t))))
+;;                 (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
+;;                 (synopsis "Non-free firmware for Intel wifi chips")
+;;                 (description "Non-free iwlwifi firmware")
+;;                 (license (license:non-copyleft
+;;                            "https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/tree/LICENCE.iwlwifi_firmware?id=HEAD"))))
 
 (define this-file
   (local-file (basename (assoc-ref (current-source-location) 'filename))
               "config.scm"))
 
-(define *lspci*
-  (let* ((port (open-pipe* OPEN_READ "lspci"))
-         (str (get-string-all port)))
-    (close-pipe port)
-    str))
+;;(define *lspci*
+;;  (let* ((port (open-pipe* OPEN_READ "lspci"))
+;;         (str (get-string-all port)))
+;;    (close-pipe port)
+;;    str))
+
+(define-public linux-stable
+	(let* ((version "5.4.3"))
+		(package
+		 (inherit linux-libre)
+		 (name "linux-stable")
+		 (version version)
+		 (source (origin
+							(method url-fetch)
+							(uri (string-append
+										"https://cdn.kernel.org/pub/linux/kernel/v5.x/"
+										"linux-" version ".tar.xz"))
+							(sha256
+							 (base32
+								"0lgfg31pgvdhkh9y4y4yh075mlk3qa6npxp7n19yxcg168pnhcb7"))))
+		 (synopsis "Linux kernel: current stable release")
+		 (description "Linux is a kernel")
+		 (license license:gpl2)
+		 (home-page "http://kernel.org/"))))
+
+(define linux-firmware-version "20191215")
+(define (linux-firmware-nonfree version)
+	(origin
+	 (method url-fetch)
+	 (uri (string-append
+				 "https://mirrors.edge.kernel.org/pub/linux/kernel/firmware/"
+				 "linux-firmware-" linux-firmware-version ".tar.xz"))
+	 (sha256
+		(base32
+		 "0mmfq2an2d58209s4pl5jnlsig285bw8i55vxp1z5h4plzs8y1n2"))))
+
+(define-public iwlwifi-firmware-nonfree
+	(package
+	 (name "iwlwifi-firmware-nonfree")
+	 (version linux-firmware-version)
+	 (source (linux-firmware-nonfree version))
+	 (build-system trivial-build-system)
+	 (arguments
+		`(#:modules ((guix build utils))
+								#:builder (begin
+														(use-modules (guix build utils))
+														(let ((source (assoc-ref %build-inputs "source"))
+																	(fw-dir (string-append %output "/lib/firmware/")))
+															(mkdir-p fw-dir)
+															(for-each (lambda (file)
+																					(copy-file file
+																										 (string-append fw-dir (basename file))))
+																				(find-files source
+																										"iwlwifi-.*\\.ucode$|LICENSE\\.iwlwifi_firmware$"))
+															#t))))
+
+	 (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
+	 (synopsis "Non-free firmware for Intel wifi chips")
+	 (description "Non-free iwlwifi firmware")
+	 (license (license:non-copyleft
+						 "https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/tree/LICENCE.iwlwifi_firmware?id=HEAD"))))
 
 (operating-system
-  (kernel (cond
-            ((string-match "Network controller: Intel Corporation Wireless 7260"
-                           *lspci*)
-             linux-nonfree)
-            (#t linux-libre)))
-  (firmware (append (list linux-firmware-iwlwifi)
-                    %base-firmware))
+;;  (kernel (cond
+;;            ((string-match "Network controller: Intel Corporation Wireless 7260"
+;;                           *lspci*)
+;;             linux-nonfree)
+;;            (#t linux-libre)))
+;;  (firmware (append (list linux-firmware-iwlwifi)
+;;                    %base-firmware))
   (locale "en_US.utf8")
   (host-name "guixbook")
   (timezone "America/New_York")
@@ -143,7 +199,12 @@
             (source
               (uuid "e817b331-0213-4c98-a2f4-8a857ed7b674"))
             (target "crypthome")
-            (type luks-device-mapping))))
+            (type luks-device-mapping))
+					(mapped-device
+					 (source
+						(uuid "06AB758872B5-0944-A421-7C34F43065DF")
+						(target "cryptmedia")
+						(type luks-device-mapping)))))
 
   (file-systems (cons* (file-system
                          (mount-point "/")
@@ -155,4 +216,9 @@
                          (device "/dev/mapper/crypthome")
                          (type "ext4")
                          (dependencies mapped-devices))
+											 (file-system
+												(mount-point "/media")
+												(device "/dev/mapper/cryptmedia")
+												(type "ext4")
+												(dependencies mapped-devices))
                        %base-file-systems)))
