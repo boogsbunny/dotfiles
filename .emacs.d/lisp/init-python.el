3e100b53164a7913e7c49c0be4bbc5ef474fb88b
Bugi Idris <boogs@venatores.group>
2021-11-13 17:43:16 -0500

    add python-black for formatting

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -2,9 +2,13 @@
 ;; Python
 ;;--------------------------------;
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(require 'python-mode nil t)
+
+;; (autoload 'python-mode "python-mode" "Python Mode." t)
+;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (setq python-shell-interpreter "python3")
+
+(add-hook 'python-mode-hook #'python-black)
 
 (provide 'init-python)
