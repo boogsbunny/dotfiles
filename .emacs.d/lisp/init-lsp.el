db586391db739b029cd3b10d8cc310ee1739f855
Bugi Abdulkarim <ba2ra@virginia.edu>
2022-07-03 13:29:58 -0400

    refactor: rip csharp

--- .emacs.d/lisp/init-lsp.el
+++ .emacs.d/lisp/init-lsp.el
@@ -7,20 +7,19 @@
 (require 'lsp-mode)
 (require 'lsp-ui)
 (require 'helm-lsp)
-(require 'company-lsp)
-(require 'lsp-pyright)
+;; (require 'company-lsp)
 
 (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
 (add-hook 'lsp-managed-mode-hook 'lsp-diagnostics-modeline-mode)
 
+(add-hook 'html-mode-hook #'lsp-deferred)
 (add-hook 'rust-mode-hook #'lsp-deferred)
-(add-hook 'csharp-mode-hook #'lsp-deferred)
 (add-hook 'js2-mode-hook #'lsp-deferred)
 (add-hook 'typescript-mode-hook #'lsp-deferred)
 (add-hook 'web-mode-hook #'lsp-deferred)
 (add-hook 'python-mode-hook #'lsp-deferred)
 
 (setq lsp-prefer-capf t
-			lsp-idle-delay 0.500)
+      lsp-idle-delay 0.500)
 
 (provide 'init-lsp)
