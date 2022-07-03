507e1ffa5e075342a94dff8f65c4a21f395fe4ad
Bugi Abdulkarim <ba2ra@virginia.edu>
2022-07-03 13:37:10 -0400

    refactor: update python config

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -2,13 +2,17 @@
 ;; Python
 ;;--------------------------------;
 
-(require 'python-mode nil t)
-
-;; (autoload 'python-mode "python-mode" "Python Mode." t)
-;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (setq python-shell-interpreter "python3")
 
-(add-hook 'python-mode-hook #'python-black)
+(setq indent-tabs-mode nil)
+(setq python-indent-offset 4)
+(setq tab-width 4)
+
+(elpy-enable)
+(add-hook 'python-mode-hook #'elpy-mode)
+(setq elpy-rpc-python-command "python3")
+
+(add-hook 'python-mode-hook #'python-black-on-save-mode)
 
 (provide 'init-python)
