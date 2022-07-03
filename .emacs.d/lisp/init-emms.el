7972af0b66ef211706b2d686526e3b7487f1c32d
Bugi Abdulkarim <ba2ra@virginia.edu>
2022-07-03 12:35:40 -0400

    refactor: point to new dir and provide

--- .emacs.d/lisp/init-emms.el
+++ .emacs.d/lisp/init-emms.el
@@ -2,14 +2,15 @@
 ;; Music
 ;;--------------------------------;
 
-
 (emms-all)
 (emms-history-load)
 
 (setq emms-player-list (list emms-player-mpv)
-      emms-source-file-default-directory (file-truename "/media/videos/")
+      emms-source-file-default-directory (file-truename "/media/personal/videos/lofi/")
       emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find
       ;; Cover thumbnails.
       emms-browser-covers 'emms-browser-cache-thumbnail-async)
 (add-to-list 'emms-player-mpv-parameters "--no-audio-display")
 (add-to-list 'emms-info-functions 'emms-info-cueinfo)
+
+(provide 'init-emms)
