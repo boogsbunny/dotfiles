8a085c822f038e4d9cccac1d3947f15b5ca126b4
Bugi Abdulkarim <ba2ra@virginia.edu>
2022-07-03 12:34:26 -0400

    fix: use whitespace mode and point to guix sbcl

--- .emacs.d/lisp/init-lisp.el
+++ .emacs.d/lisp/init-lisp.el
@@ -5,12 +5,20 @@
 (require 'init-lispy)
 
 (add-hook 'lisp-mode-hook 'boogs/init-lispy)
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "$HOME/.guix-profile/lib/sbcl")
+
+(whitespace-mode 1)
+
+(when (fboundp 'rainbow-delimiters-mode)
+  (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode))
 
 ;; ;;--------------------------------
 
 (when (require 'helm-sly nil 'noerror)
   (global-helm-sly-mode)
   (add-to-list 'helm-source-names-using-follow "Lisp xrefs"))
+
+(with-eval-after-load 'sly
+  (require 'init-sly))
 
 (provide 'init-lisp)
