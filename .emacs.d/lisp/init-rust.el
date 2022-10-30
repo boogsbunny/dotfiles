04924684791989e85e2bb2fa957d56f519c05092
Bugi Abdulkarim <ba2ra@virginia.edu>
2022-10-30 15:21:48 -0400

    feat: add tree sitter to rust

--- .emacs.d/lisp/init-rust.el
+++ .emacs.d/lisp/init-rust.el
@@ -7,6 +7,8 @@
 (define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
 
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'rust-mode-hook #'tree-sitter-mode)
+(add-hook 'rust-mode-hook #'tree-sitter-hl-mode)
 
 (require 'init-lsp)
 
