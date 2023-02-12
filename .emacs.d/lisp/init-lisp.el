b5411d415446f4b3ee0717227035b1a3052783b6
Bugi Abdulkarim <ba2ra@virginia.edu>
2023-02-12 09:39:24 -0500

    feat: add auto complete for lisp

--- .emacs.d/lisp/init-lisp.el
+++ .emacs.d/lisp/init-lisp.el
@@ -7,12 +7,13 @@
 (add-hook 'lisp-mode-hook 'boogs/init-lispy)
 (setq inferior-lisp-program "$HOME/.guix-profile/lib/sbcl")
 
+(add-hook 'lisp-mode-hook
+          (function (lambda () (setq fill-column (string-to-number "100")))))
+
 (whitespace-mode 1)
 
 (when (fboundp 'rainbow-delimiters-mode)
   (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode))
-
-;; ;;--------------------------------
 
 (when (require 'helm-sly nil 'noerror)
   (global-helm-sly-mode)
@@ -20,5 +21,9 @@
 
 (with-eval-after-load 'sly
   (require 'init-sly))
+
+(require 'company)
+(add-hook 'lisp-mode-hook #'company-mode)
+(define-key lisp-mode-map (kbd "<tab>") 'sly-mrepl-indent-and-complete-symbol)
 
 (provide 'init-lisp)
