fe191ea261872128f5a23bc71071f5c52142cc0f
Bugi Abdulkarim <ba2ra@virginia.edu>
2023-02-12 09:48:48 -0500

    refactor: use minimalistic emms config

--- .emacs.d/lisp/init-emms.el
+++ .emacs.d/lisp/init-emms.el
@@ -2,15 +2,17 @@
 ;; Music
 ;;--------------------------------;
 
-(emms-all)
+(emms-minimalistic)
 (emms-history-load)
+(emms-mode-line-disable)
 
 (setq emms-player-list (list emms-player-mpv)
-      emms-source-file-default-directory (file-truename "/media/personal/videos/lofi/")
+      emms-source-file-default-directory "/media/personal/videos/lofi"
       emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find
       ;; Cover thumbnails.
       emms-browser-covers 'emms-browser-cache-thumbnail-async)
+
 (add-to-list 'emms-player-mpv-parameters "--no-audio-display")
 (add-to-list 'emms-info-functions 'emms-info-cueinfo)
 
-(provide 'init-emms)
+ (provide 'init-emms)
