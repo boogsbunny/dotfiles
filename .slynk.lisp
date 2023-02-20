564db35c5ddd96387bfea1ba79b404cb7d19067e
Boogs <ba2ra@virginia.edu>
2023-02-20 16:32:40 -0500

    refactor: permit long strings in sly

--- .slynk.lisp
+++ .slynk.lisp
@@ -2,3 +2,5 @@
 (let ((askpass (format nil "~a/.local/bin/emacs-askpass" (uiop:getenv "HOME"))))
   (when (uiop:file-exists-p askpass)
     (setf (uiop:getenv "SUDO_ASKPASS") askpass)))
+
+(setf (cdr (assoc 'slynk:*string-elision-length* slynk:*slynk-pprint-bindings*)) nil)
