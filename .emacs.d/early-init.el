3bc568075e7ae8523bb734b99dfbc8e3f5b534bc
Boogs <ba2ra@virginia.edu>
2023-11-13 22:07:39 -0500

    refactor: separate init file to early init and init

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,38 @@
+;;--------------------------------------------------------------------
+;; early-init
+;;--------------------------------------------------------------------
+
+;; additional config in 'lisp' folder
+(add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
+;; site-lisp folder for local packages on MacOSX
+;; (add-to-list 'load-path (expand-file-name "site-lisp/" user-emacs-directory))
+
+;; separate user files with cache files
+(setq user-emacs-directory "~/.cache/emacs/")
+
+;; site-lisp folder for local packages on GNU/Linux
+(defun boogs/package-refresh-load-path (path)
+  "Add every non-hidden sub-folder of PATH to `load-path'."
+  (when (file-directory-p path)
+    (dolist (dir (directory-files path t "^[^\\.]"))
+      (when (file-directory-p dir)
+        (setq load-path (add-to-list 'load-path dir))
+        (dolist (subdir (directory-files dir t "^[^\\.]"))
+          (when (file-directory-p subdir)
+            (setq load-path (add-to-list 'load-path subdir))))))))
+
+(let ((site-lisp (expand-file-name "site-lisp/" "~/.local/share/emacs/")))
+  (add-to-list 'load-path site-lisp)
+  (boogs/package-refresh-load-path site-lisp))
+
+(setq evil-want-keybinding nil
+      evil-want-integration t)
+
+(setq history-length 100)
+(put 'minibuffer-history 'history-length 50)
+(put 'evil-ex-history 'history-length 50)
+(put 'kill-ring 'history-length 25)
+
+(setq max-lisp-eval-depth 10000)
+
+(require 'init-packages)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,39 +1,12 @@
-;;------------------------------------------------------------
-;; INIT
-;;------------------------------------------------------------
+;;--------------------------------------------------------------------
+;; init
+;;--------------------------------------------------------------------
 
-;; additional config in 'lisp' folder
-(add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
-;; site-lisp folder for local packages on MacOSX
-;; (add-to-list 'load-path (expand-file-name "site-lisp/" user-emacs-directory))
-
-;; separate user files with cache files
-(setq user-emacs-directory "~/.cache/emacs/")
-
-;; site-lisp folder for local packages on GNU/Linux
-(defun boogs/package-refresh-load-path (path)
-  "Add every non-hidden sub-folder of PATH to `load-path'."
-  (when (file-directory-p path)
-    (dolist (dir (directory-files path t "^[^\\.]"))
-      (when (file-directory-p dir)
-        (setq load-path (add-to-list 'load-path dir))
-        (dolist (subdir (directory-files dir t "^[^\\.]"))
-          (when (file-directory-p subdir)
-            (setq load-path (add-to-list 'load-path subdir))))))))
-
-(let ((site-lisp (expand-file-name "site-lisp/" "~/.local/share/emacs/")))
-  (add-to-list 'load-path site-lisp)
-  (boogs/package-refresh-load-path site-lisp))
-
-(require 'init-packages)
-(require 'init-defuns)
+(require 'init-modeline)
 (require 'init-defaults)
 (require 'init-styles)
-
-;;------------------------------------------------------------
-;; CONFIGS
-;;------------------------------------------------------------
-
+(require 'init-defuns)
+(require 'init-web)
 (require 'functions)
 
 (with-eval-after-load 'dired (require 'init-dired))
@@ -42,9 +15,10 @@
 (with-eval-after-load 'erc (require 'init-erc))
 (with-eval-after-load 'tramp (require 'init-tramp))
 
-(setq evil-want-keybinding nil
-      evil-want-integration t)
 (when (require 'evil nil t) (require 'init-evil))
+
+(when (fboundp 'pdf-view-mode)
+  (require 'init-pdf))
 
 (when (fboundp 'rainbow-delimiters-mode)
   (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
@@ -53,8 +27,6 @@
 (when (require 'rainbow-mode nil t)
   (dolist (hook '(css-mode-hook html-mode-hook sass-mode-hook js-mode-hook js2-mode-hook typescript-mode python-mode))
     (add-hook hook 'rainbow-mode)))
-
-(require 'init-web)
 
 (when (require 'company nil t)
   (setq company-idle-delay nil))
@@ -69,7 +41,7 @@
 (with-eval-after-load 'magit (require 'init-magit))
 (with-eval-after-load 'eww (require 'init-eww))
 
-(when (require 'helm-config nil t) (require 'init-helm))
+(with-eval-after-load 'helm (require 'init-helm))
 
 (with-eval-after-load 'notmuch (require 'init-notmuch))
 (autoload 'helm-notmuch-switch "notmuch")
@@ -84,9 +56,9 @@
     (add-to-list 'company-backends 'company-restclient)
     (add-hook 'restclient-mode-hook 'company-mode)
     (define-key restclient-mode-map (kbd "M-<tab>") (if (require 'helm-company nil t)
-                                                        'helm-company
-                                                      'company-complete))))
-;;; System packages
+                                                       'helm-company
+                                                     'company-complete))))
+;; System packages
 (global-set-key (kbd "C-x c #") 'helm-system-packages)
 
 (with-eval-after-load 'lisp-mode (require 'init-lisp))
@@ -109,24 +81,24 @@
 (with-eval-after-load 'slack (require 'init-slack))
 
 (with-eval-after-load 'transmission
-  ;; `transmission' will fail to start and will not run any hook if the daemon
-  ;; is not up yet.
-  ;; We need to advice the function :before to guarantee it starts.
+  ; `transmission' will fail to start and will not run any hook if the daemon
+  ; is not up yet.
+  ; We need to advice the function :before to guarantee it starts.
   (defun boogs/transmission-start-daemon ()
     (unless (member "transmission-da"
-                    (mapcar
-                     (lambda (pid) (alist-get 'comm (process-attributes pid)))
-                     (list-system-processes)))
+                   (mapcar
+                    (lambda (pid) (alist-get 'comm (process-attributes pid)))
+                    (list-system-processes)))
       (call-process "transmission-daemon")
       (sleep-for 1)))
   (advice-add 'transmission :before 'boogs/transmission-start-daemon)
   (setq transmission-refresh-modes '(transmission-mode transmission-files-mode transmission-info-mode transmission-peers-mode)
-        transmission-refresh-interval 1))
+                                        transmission-refresh-interval 1))
 
 
-;;------------------------------------------------------------
-;; CLEANUP
-;;------------------------------------------------------------
+;;--------------------------------------------------------------------
+;; cleanup
+;;--------------------------------------------------------------------
 
 ;; remove `customize' clutter
 (setq custom-file
