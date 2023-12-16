2f470ee9414ed539239f792cea9ecd1af5ba814c
Boogs <ba2ra@virginia.edu>
2023-12-16 14:22:57 -0500

    refactor: separate company and yas configs from eglot

--- .emacs.d/lisp/init-eglot.el
+++ .emacs.d/lisp/init-eglot.el
@@ -12,18 +12,4 @@
 (define-key eglot-mode-map (kbd "C-c e f p") #'flymake-goto-prev-error)
 (define-key eglot-mode-map (kbd "C-c e r") #'eglot-rename)
 
-;; auto-completion
-(require 'company)
-(add-hook 'go-mode-hook #'company-mode)
-(require 'yasnippet)
-(add-hook 'go-mode-hook #'yas-minor-mode)
-
-(setq company-minimum-prefix-length 1
-      company-idle-delay 0.1
-      company-show-numbers nil
-      company-require-match nil
-      company-dabbrev-ignore-case t
-      company-dabbrev-code-ignore-case t
-      completion-ignore-case t)
-
 (provide 'init-eglot)
