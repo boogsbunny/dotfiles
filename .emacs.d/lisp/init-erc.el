6d37e5e12ae90635fa275686e893296992a3b33a
Boogs <ba2ra@virginia.edu>
2024-08-04 17:07:43 -0400

    refactor: update erc login and add notifications

--- .emacs.d/lisp/init-erc.el
+++ .emacs.d/lisp/init-erc.el
@@ -4,17 +4,18 @@
 
 (require 'erc-image)
 (require 'patch-erc)
+(require 'erc-desktop-notifications)
 
 (add-to-list 'erc-sasl-server-regexp-list "irc\\.libera\\.chat")
 
-(setq erc-nick "boogs"
-      erc-prompt-for-password nil
+(setq erc-nick "boogsbunny"
+      erc-prompt-for-password t
       erc-auto-query 'bury
       erc-join-buffer 'bury
       erc-server-reconnect-timeout 10
       erc-server-reconnect-attemps 5
       erc-image-inline-rescale 400
       erc-autojoin-channels-alist '(("irc.libera.chat" "#commonlisp" "#lispweb" "#emacs" "#guix"))
-      erc-modules '(autojoin image hl-nicks))
+      erc-modules '(autojoin image hl-nicks networks notifications))
 
 (provide 'init-erc)
