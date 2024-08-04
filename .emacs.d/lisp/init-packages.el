25a80c6ecc2f7944bc33595cffa93ce080dae5f4
Boogs <ba2ra@virginia.edu>
2024-08-04 16:53:16 -0400

    fix: remove org elpa url and add gnu/nongnu urls for pkgs

--- .emacs.d/lisp/init-packages.el
+++ .emacs.d/lisp/init-packages.el
@@ -4,8 +4,9 @@
 
 (when (>= emacs-major-version 24)
   (require 'package)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (package-initialize))
 
 (setq package-selected-packages
