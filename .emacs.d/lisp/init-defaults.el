0b23fbcb9b5db6f3a851dfe878d13441ec34e273
Boogs <ba2ra@virginia.edu>
2024-08-05 18:37:59 -0400

    feat: don't use olivetti mode on laptop

--- .emacs.d/lisp/init-defaults.el
+++ .emacs.d/lisp/init-defaults.el
@@ -240,16 +240,22 @@
   "Check if only a single buffer is visible in the current frame."
   (<= (length (window-list)) 1))
 
+(defun laptop-screen-p ()
+  (and
+   (= (x-display-pixel-width) 3000)
+   (= (x-display-pixel-height) 2000)))
+
 (defun maybe-enable-olivetti-mode ()
   "Enable olivetti mode if on monitor and only a single buffer is visible."
-  (if (or (and (derived-mode-p 'prog-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'org-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'yaml-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'eww-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'slack-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'magit-status-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'comint-mode) (single-buffer-visible-p))
-          (and (derived-mode-p 'erc-mode) (single-buffer-visible-p)))
+  (if (and (not (laptop-screen-p))
+           (or (and (derived-mode-p 'prog-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'org-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'yaml-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'eww-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'slack-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'magit-status-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'comint-mode) (single-buffer-visible-p))
+               (and (derived-mode-p 'erc-mode) (single-buffer-visible-p))))
       (olivetti-mode 1)
     (olivetti-mode -1)))
 
