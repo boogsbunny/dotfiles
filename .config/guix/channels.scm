b1cf35cd13682072273811a7a55c9fcfa9d505af
Boogs <ba2ra@virginia.edu>
2024-12-23 00:54:05 -0500

    feat: add personal guix channel to have hidden c libs in path

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,4 +1,7 @@
 (cons* (channel
-         (name 'nonguix)
-         (url "https://gitlab.com/nonguix/nonguix"))
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix"))
+       (channel
+        (name 'boogs)
+        (url "https://github.com/boogsbunny/boogs"))
        %default-channels)
