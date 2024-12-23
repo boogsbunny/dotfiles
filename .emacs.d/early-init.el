7f54367b23a44a6c89d6f79811c3c4963e7e6215
Boogs <ba2ra@virginia.edu>
2024-12-23 00:56:36 -0500

    refactor: don't get stuck on emacs native compilation loop on startup

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -46,4 +46,14 @@
 
 (setq xref-search-program 'ripgrep)
 
+(defun fixed-native-compile-async-skip-p (native-compile-async-skip-p file load selector)
+  (let* ((naive-elc-file (file-name-with-extension file "elc"))
+         (elc-file       (replace-regexp-in-string
+                          "\\.el\\.elc$" ".elc" naive-elc-file)))
+    (or (gethash elc-file comp--no-native-compile)
+        (funcall native-compile-async-skip-p file load selector))))
+
+(advice-add 'native-compile-async-skip-p
+            :around 'fixed-native-compile-async-skip-p)
+
 (require 'init-packages)
