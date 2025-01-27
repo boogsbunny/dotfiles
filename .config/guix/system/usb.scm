6d64e80abbf2ce93d93ed08b6e302cbfe5fa9a38
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-01-26 21:48:54 -0500

    feat: add guix usb disk image config

---
+++ .config/guix/system/usb.scm
@@ -0,0 +1,53 @@
+;; guix system -L ~/.config/guix/system image -r guix.img ~/.config/guix/system/usb.scm
+;;
+;; then write it to the usb:
+;;
+;; sudo dd if=/path/to/your-image.img of=/dev/sdX bs=4M status=progress conv=fsync
+;; sync
+(define-module (usb)
+  #:use-module (srfi srfi-1)
+  #:use-module (default)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (gnu))
+
+(use-service-modules
+ desktop
+ xorg)
+
+(use-package-modules bootloaders)
+
+(define-public %usb/services
+  (cons*
+   (remove
+    (lambda (service)
+      (member (service-kind service)
+              (list bluetooth-service-type
+                    cups-pk-helper-service-type
+                    geoclue-service-type
+                    gdm-service-type
+                    gnome-desktop-service-type)))
+    %boogs/services)))
+
+(operating-system
+  (inherit %boogs/os)
+
+  (bootloader (bootloader-configuration
+               (bootloader grub-bootloader)
+               (targets '("/dev/sdb"))))
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (append (list iwlwifi-firmware)
+                    %boogs/firmware))
+
+  (file-systems (cons* (file-system
+                         (device (file-system-label "guix"))
+                         (mount-point "/")
+                         (type "ext4"))
+                       (file-system
+                         (device "none")
+                         (mount-point "/tmp")
+                         (type "tmpfs")
+                         (check? #f))
+                       %base-file-systems))
+  (services %usb/services))
