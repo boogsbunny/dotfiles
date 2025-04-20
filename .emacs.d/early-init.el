d8885bd5515632700084328e219cdb0e5de2dbad
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-04-20 07:47:44 -0400

    refactor(emacs): ignore el.gz files in native comp

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -49,7 +49,7 @@
 (defun fixed-native-compile-async-skip-p (native-compile-async-skip-p file load selector)
   (let* ((naive-elc-file (file-name-with-extension file "elc"))
          (elc-file       (replace-regexp-in-string
-                          "\\.el\\.elc$" ".elc" naive-elc-file)))
+                          "\\.el\\el.gz\\.elc$" ".elc" naive-elc-file)))
     (or (gethash elc-file comp--no-native-compile)
         (funcall native-compile-async-skip-p file load selector))))
 
