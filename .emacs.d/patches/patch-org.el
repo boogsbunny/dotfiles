3ec0fc5f55aefbb8d9e990c539fcea6c2496254f
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-04-20 07:55:31 -0400

    fix(emacs): add helper fn and settings for defaults in new org version

--- .emacs.d/patches/patch-org.el
+++ .emacs.d/patches/patch-org.el
@@ -1,3 +1,28 @@
+;; Helper function to convert existing Org documents to the new
+;; standard tab width.
+(defun org-compat-adjust-tab-width-in-buffer (old-width)
+  "Adjust visual indentation from `tab-width' equal OLD-WIDTH to 8."
+  (interactive "nOld `tab-width': ")
+  (cl-assert (derived-mode-p 'org-mode))
+  (unless (= old-width 8)
+    (org-with-wide-buffer
+     (goto-char (point-min))
+     (let (bound
+           (repl (if (< old-width 8)
+                     (make-string old-width ?\s)
+                   (concat "\t" (make-string (- old-width 8) ?\s)))))
+       (while (re-search-forward "^ *\t" nil t)
+         (skip-chars-forward " \t")
+         (setq bound (point-marker))
+         (forward-line 0)
+         (while (search-forward "\t" bound t)
+           (replace-match repl)))))))
+
+(defun boogs/org-setup ()
+  (setq tab-width 8))
+
+(add-hook 'org-mode-hook #'boogs/org-setup)
+
 (setq org-modern-label-border 1
       org-modern-variable-pitch nil
       org-modern-timestamp t
