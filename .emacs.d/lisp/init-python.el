0d9f751d260c8a642246169dbe5c0329e68b6b8f
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-08-24 11:04:46 -0400

    refactor(python): use tree-sitter

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -2,17 +2,17 @@
 ;; python
 ;;--------------------------------------------------------------------
 
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-(setq python-shell-interpreter "python3")
+(setq major-mode-remap-alist
+      '((python-mode . python-ts-mode)))
 
+(add-to-list 'interpreter-mode-alist '("python" . python-ts-mode))
+
+(setq python-shell-interpreter "python3")
 (setq indent-tabs-mode nil)
 (setq python-indent-offset 4)
 (setq tab-width 4)
 
-(elpy-enable)
-(add-hook 'python-mode-hook #'elpy-mode)
-(setq elpy-rpc-python-command "python3")
-
-(add-hook 'python-mode-hook #'python-black-on-save-mode)
+(add-hook 'python-ts-mode-hook 'eglot-ensure)
+(add-hook 'before-save-hook 'python-black-on-save-mode-enable-dwim)
 
 (provide 'init-python)
