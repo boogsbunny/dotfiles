a38f3d8bfa9313aa2861867d16b55aaebad8e5ed
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-11-08 09:06:03 -0500

    refactor(emacs): update tramp config for better performance

--- .emacs.d/lisp/init-tramp.el
+++ .emacs.d/lisp/init-tramp.el
@@ -2,17 +2,42 @@
 ;; tramp
 ;;--------------------------------------------------------------------
 
-(setq vc-ignore-dir-regexp
+(setq projectile--mode-line "Projectile"
+
+      remote-file-name-inhibit-auto-save-visited t
+      remote-file-name-inhibit-cache nil
+      remote-file-name-inhibit-locks t
+
+      tramp-copy-size-limit 10000
+      tramp-inline-compress-start-size 1000
+      tramp-use-scp-direct-remote-copying t
+      tramp-use-ssh-controlmaster-options nil
+      tramp-ssh-controlmaster-options ""
+      tramp-verbose 1
+
+      vc-ignore-dir-regexp
       (format "\\(%s\\)\\|\\(%s\\)"
               vc-ignore-dir-regexp
               tramp-file-name-regexp)
-      tramp-inline-compress-start-size 1000
-      tramp-copy-size-limit 10000
       vc-handled-backends '(Git)
-      tramp-verbose 1
-      remote-file-name-inhibit-cache nil
-      tramp-use-ssh-controlmaster-options nil
-      tramp-ssh-controlmaster-options ""
-      projectile--mode-line "Projectile")
+      )
+
+(connection-local-set-profile-variables
+ 'remote-direct-async-process
+ '((tramp-direct-async-process . t)))
+
+(connection-local-set-profiles
+ '(:application tramp :protocol "scp")
+ 'remote-direct-async-process)
+
+(setq magit-tramp-pipe-stty-settings 'pty)
+
+(with-eval-after-load 'compile
+  (remove-hook 'compilation-mode-hook
+               #'tramp-compile-disable-ssh-controlmaster-options))
+
+(remove-hook 'find-file-hook 'forge-bug-reference-setup)
+
+(require 'init-consult-tramp)
 
 (provide 'init-tramp)
