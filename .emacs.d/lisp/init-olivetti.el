ddb381f2ef0761c830035f0c09d529e1633c8a4e
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-11-13 04:09:30 -0500

    fix(emacs): remove olivetti hot path on buffer kills

--- .emacs.d/lisp/init-olivetti.el
+++ .emacs.d/lisp/init-olivetti.el
@@ -2,38 +2,49 @@
 ;; olivetti
 ;;--------------------------------------------------------------------
 
+(require 'olivetti)
+
 (defun single-buffer-visible-p ()
-  "Check if only a single buffer is visible in the current frame."
-  (<= (length (window-list)) 1))
+  "Non-nil if only a single window is visible in the selected frame."
+  (<= (length (window-list (selected-frame))) 1))
 
 (defun laptop-screen-p ()
   (and
    (<= (x-display-pixel-width) 3000)
    (<= (x-display-pixel-height) 2000)))
 
-(defun maybe-enable-olivetti-mode ()
-  "Enable olivetti mode if on monitor and only a single buffer is visible."
-  (if (and (not (laptop-screen-p))
-           (or (and (derived-mode-p 'comint-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'dired-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'erc-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'eww-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'json-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'magit-status-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'markdown-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'text-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'elfeed-show-mode) (single-buffer-visible-p))
-               ;; (and (derived-mode-p 'notmuch-show-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'org-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'slack-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'yaml-mode) (single-buffer-visible-p))
-               (and (derived-mode-p 'prog-mode) (single-buffer-visible-p))))
-      (olivetti-mode 1)
-    (olivetti-mode -1)))
+(defconst boogs/olivetti-eligible-modes
+  '(comint-mode
+    dired-mode
+    elfeed-show-mode
+    erc-mode
+    eww-mode
+    json-mode
+    magit-status-mode
+    markdown-mode
+    org-mode
+    prog-mode
+    slack-mode
+    text-mode
+    yaml-mode)
+  "Major modes in which Olivetti may be auto-enabled.")
 
-(add-hook 'window-configuration-change-hook 'maybe-enable-olivetti-mode)
-(add-hook 'kill-buffer-hook 'maybe-enable-olivetti-mode)
-(add-hook 'after-change-major-mode-hook 'maybe-enable-olivetti-mode)
+(defun boogs/olivetti-should-enable-p ()
+  (and (not (laptop-screen-p))
+       (single-buffer-visible-p)
+       (apply #'derived-mode-p boogs/olivetti-eligible-modes)))
+
+(defun maybe-enable-olivetti-mode ()
+  "Enable/disable Olivetti only when the desired state changes."
+  (let ((want (boogs/olivetti-should-enable-p)))
+    (unless (eq want olivetti-mode)
+      (olivetti-mode (if want 1 -1)))))
+
+(add-hook 'window-configuration-change-hook #'maybe-enable-olivetti-mode)
+(add-hook 'after-change-major-mode-hook #'maybe-enable-olivetti-mode)
+
+;; Avoid expensive margin work during buffer kills.
+(remove-hook 'kill-buffer-hook #'maybe-enable-olivetti-mode)
 
 (setq olivetti-body-width 0.65
       olivetti-minimum-body-width 140
