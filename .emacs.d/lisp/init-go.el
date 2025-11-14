276d0fd9f0a00520f362457fa0608f9ce1f964cb
Bugrahan Abdulkarim <boogs@virginia.edu>
2025-11-14 06:20:45 -0500

    refactor(emacs): remove custom olivetti width in `go-mode`

--- .emacs.d/lisp/init-go.el
+++ .emacs.d/lisp/init-go.el
@@ -9,7 +9,6 @@
 (add-to-list 'exec-path "~/go/bin")
 (setq gofmt-command "goimports")
 
-(add-hook 'go-mode-hook (function (lambda () (setq olivetti-body-width 0.65))))
 (add-hook 'go-mode-hook (function (lambda () (setq fill-column 120))))
 
 ;; (add-hook 'go-mode-hook #'tree-sitter-mode)
