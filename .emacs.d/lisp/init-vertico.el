8dbff07dd12ad39fc802e3249ffbf840214483e8
Bugrahan Abdulkarim <boogs@virginia.edu>
2026-05-08 09:07:51 -0400

    feat(emacs/vertico): remap group navigation to `C-n`/`C-p`

--- .emacs.d/lisp/init-vertico.el
+++ .emacs.d/lisp/init-vertico.el
@@ -13,8 +13,9 @@
 (define-key vertico-map (kbd "C-j") 'vertico-next)
 (define-key vertico-map (kbd "C-k") 'vertico-previous)
 
-(define-key vertico-map (kbd "M-n") 'vertico-next-group)
-;; (define-key vertico-map (kbd "M-p") 'vertico-previous-group)
+(define-key vertico-map (kbd "C-n") 'vertico-next-group)
+(define-key vertico-map (kbd "C-p") 'vertico-previous-group)
+
 (define-key vertico-map (kbd "M-j") 'vertico-next-group)
 (define-key vertico-map (kbd "M-k") 'vertico-previous-group)
 
@@ -23,6 +24,5 @@
 (define-key vertico-map (kbd "TAB") 'vertico-directory-enter)
 
 (vertico-mode)
-;; (vertico-buffer-mode)
 
 (provide 'init-vertico)
