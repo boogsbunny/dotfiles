2bc7ff9ad911b4c4aba72c931195f2e629ab9850
Bugrahan Abdulkarim <boogs@virginia.edu>
2026-05-08 08:25:37 -0400

    feat(emacs): add keybinding for org-roam project finder (`C-c n p`)

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -182,6 +182,7 @@
 (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
 (global-set-key (kbd "C-c n f") 'org-roam-node-find)
 (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
+(global-set-key (kbd "C-c n p") 'boogs/org-roam-find-project)
 
 (global-set-key (kbd "C-c n I") 'org-roam-node-insert-immediate)
 (global-set-key (kbd "C-c n t") 'boogs/org-roam-capture-task)
