ff83c025e3bae2210741d275023c9235004e0ed1
Bugrahan Abdulkarim <boogs@virginia.edu>
2026-05-08 08:48:54 -0400

    feat(emacs/corfu): add `C-n`/`C-p` bindings and adjust auto-delay

--- .emacs.d/lisp/init-corfu.el
+++ .emacs.d/lisp/init-corfu.el
@@ -7,13 +7,19 @@
 (define-key corfu-map (kbd "C-j") 'corfu-next)
 (define-key corfu-map (kbd "C-k") 'corfu-previous)
 
+(define-key corfu-map (kbd "C-n") 'corfu-next)
+(define-key corfu-map (kbd "C-p") 'corfu-previous)
+
 (define-key corfu-map (kbd "M-n") 'corfu-next)
 (define-key corfu-map (kbd "M-p") 'corfu-previous)
 
 (setq corfu-cycle t
-      corfu-preview-current 'insert)
+      corfu-preview-current 'insert
+      corfu-auto-delay 0.4)
 
 (setq-default corfu-auto t)
+
+(keymap-unset corfu-map "RET")
 
 (global-corfu-mode)
 
