5e9795f7fe44e9fb40ae655f4fe9001d53117dd4
Bugrahan Abdulkarim <boogs@venatores.group>
2026-05-26 16:48:33 -0400

    fix(emacs): restore SQLi submit by rebinding RET in evil

--- .emacs.d/lisp/init-sql.el
+++ .emacs.d/lisp/init-sql.el
@@ -5,6 +5,15 @@
 ;; remove default login parameters
 (setq sql-postgres-login-params nil)
 
+;; evil-collection binds RET to newline in insert state for comint
+;; REPLs by default; force SQLi insert RET to submit input.
+(with-eval-after-load 'sql
+  (with-eval-after-load 'evil
+    (evil-define-key 'insert sql-interactive-mode-map
+      (kbd "RET") #'comint-send-input
+      (kbd "C-m") #'comint-send-input
+      [return] #'comint-send-input)))
+
 ;; define connections
 (setq sql-connection-alist
       '((localhost (sql-product 'postgres) (sql-database (concat "postgresql://"
