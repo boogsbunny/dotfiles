28525c0b140fbf9031e4bd36359bfdd86d8677f3
Bugrahan Abdulkarim <boogs@venatores.group>
2026-06-20 07:59:30 -0400

    refactor(emacs): update rust config

--- .emacs.d/lisp/init-rust.el
+++ .emacs.d/lisp/init-rust.el
@@ -8,5 +8,28 @@
 
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
+(with-eval-after-load 'flycheck-rust
+  (setq flycheck-rust-cargo-check-args
+        '("--all-targets" "--all-features")))
+
+(defun boogs/rust-cargo-root ()
+  "Return the nearest Cargo workspace/package root."
+  (or (locate-dominating-file default-directory "Cargo.lock")
+      (locate-dominating-file default-directory "Cargo.toml")))
+
+(defun boogs/rust-analyzer-contact (_interactive)
+  "Start rust-analyzer for the nearest Cargo workspace."
+  (let* ((root (boogs/rust-cargo-root))
+         (manifest (and root (expand-file-name "Cargo.toml" root))))
+    `("rust-analyzer"
+      :initializationOptions
+      (:linkedProjects [,manifest]
+       :cargo (:targetDir "target/rust-analyzer")
+       :procMacro (:enable t)
+       :check (:workspace t)))))
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '((rust-ts-mode rust-mode) . boogs/rust-analyzer-contact)))
 
 (provide 'init-rust)
